035b68995f1d360f6c7d9f44d2c362fb09fdcd61
Edgar Yeganyan <edgar.yeganyan@russianschool.com>
2024-02-28 15:21:10 +0400

    implement PrivateTalk service

--- src/app/service/private-talk.service.ts
+++ src/app/service/private-talk.service.ts
@@ -9,17 +9,46 @@ import {User} from "../model/user";
 export class PrivateTalkService {
   private privateTalks: {[key: PrivateTalkId]: GroupId} = {};
 
+  private static nextId: PrivateTalkId = 0;
+
   constructor(
     private groupingService: GroupingService
   ) {}
 
-  public startPrivateTalk(user: User): number {
-    return 0;
+  public async startPrivateTalk(user: User): Promise<PrivateTalkId> {
+    const privateTalkId: PrivateTalkId = PrivateTalkService.nextId;
+    const groupId: GroupId = this.groupingService.createGroup();
+
+    this.privateTalks[privateTalkId] = groupId;
+    await this.groupingService.addUserToGroup(groupId, user);
+
+    return privateTalkId;
   }
 
-  public joinPrivateTalk(privateTalkId: PrivateTalkId, user: User): void {}
+  public async joinPrivateTalk(privateTalkId: PrivateTalkId, user: User): Promise<void> {
+    const groupId: GroupId = this.privateTalks[privateTalkId];
+    if (groupId) {
+      await this.groupingService.addUserToGroup(groupId, user);
+    }
+  }
 
-  public leavePrivateTalk(privateTalkId: PrivateTalkId, user: User): void {}
+  public async leavePrivateTalk(privateTalkId: PrivateTalkId, user: User): Promise<void> {
+    const groupId: GroupId = this.privateTalks[privateTalkId];
+    if (groupId) {
+      await this.groupingService.removeUserFromGroup(groupId, user);
+    }
+  }
 
-  public endPrivateTalk(privateTalkId: PrivateTalkId): void {}
+  public async endPrivateTalk(privateTalkId: PrivateTalkId): Promise<void> {
+    const groupId: GroupId = this.privateTalks[privateTalkId];
+    if (groupId) {
+      await this.groupingService.deleteGroup(groupId);
+      delete this.privateTalks[privateTalkId];
+    }
+  }
+
+  public async endAllPrivateTalks(): Promise<void> {
+    await this.groupingService.deleteAllGroups();
+    this.privateTalks = {};
+  }
 }
